0ef27e13ab8ead90fcd3a66442355d1b8e3c64c2
bzuker <bzuker@medallia.com>
2020-08-11 13:06:21 -0300

    add cafecito

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import { Grid, Segment } from "semantic-ui-react";
+import { Grid, Segment, Menu } from "semantic-ui-react";
 import JoinGame from "../components/JoinGame";
 import CreateGame from "../components/CreateGame";
 
 export default () => (
+  <>
   <Segment placeholder>
     <Grid columns={2} stackable divided>
       <Grid.Row verticalAlign="middle">
@@ -15,4 +16,9 @@ export default () => (
       </Grid.Row>
     </Grid>
   </Segment>
+  <Menu fixed='bottom' style={{padding: 10, alignItems: 'center'}}>
+    <div>By <a style={{marginLeft: 2}}href='https://mobile.twitter.com/Brian_Zuker' target='_blank' rel='noopener'>@Brian_Zuker</a></div>
+    <a style={{marginLeft: 'auto'}} href='https://cafecito.app/bzuker' rel='noopener' target='_blank'><img srcset='https://cdn.cafecito.app/imgs/buttons/button_5.png 1x, https://cdn.cafecito.app/imgs/buttons/button_5_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_5_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_5.png' alt='Invitame un café en cafecito.app' /></a>
+  </Menu>
+  </>
 );
